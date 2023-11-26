71c1ad3df39b23b496c59ddfcc30c819590ca7c1
kronek <kronkowski.daniel@gmail.com>
2023-11-26 19:55:50 +0100

    #3 add home view to main page, add base.html with header and footer

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from mybook.views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
+    path('mybook/', include('mybook.urls'))
 ]
