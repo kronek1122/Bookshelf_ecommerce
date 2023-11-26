9e794a86f625c4846dab5eef24f4d03131bee967
kronek <kronkowski.daniel@gmail.com>
2023-11-26 18:03:33 +0100

    add functions Genre, Author, Book in models, register func in admin panel, migration

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mybook.apps.MybookConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mybook/models.py
+++ mybook/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+
+    title = models.CharField()
+    author = models.ForeignKey('Author',on_delete=models.SET_NULL,null=True)
+    isbn = models.CharField('ISBN',max_length=13,unique=True)
+    genre = models.ManyToManyField(Genre)
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.last_name} , {self.first_name}"
