a153cc6d46715ff54e5c571e69ac4ad913cd0ab8
kronek <kronkowski.daniel@gmail.com>
2023-12-02 17:48:27 +0100

    #3 add signup views and templates

--- mybook/urls.py
+++ mybook/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'mybook'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('my_view', views.user_view, name='my_view')
+    path('my_view', views.user_view, name='my_view'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- mybook/views.py
+++ mybook/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
 
 
 def home(request):
@@ -9,3 +12,9 @@ def home(request):
 @login_required
 def user_view(request):
     return render(request, 'mybook/user_view.html')
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'mybook/signup.html'
