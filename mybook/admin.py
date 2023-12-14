fdddf76d6c0dc641a2a74e5f0b9eec31753596bd
kronek <kronkowski.daniel@gmail.com>
2023-12-14 20:06:08 +0100

    #1 add UserShelf and ReadBook models to admin site

--- mybook/admin.py
+++ mybook/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Genre, Author, Book
+from .models import Genre, Author, Book, UserShelf, ReadBook
 
 
 admin.site.register(Genre)
 admin.site.register(Author)
 admin.site.register(Book)
+admin.site.register(UserShelf)
+admin.site.register(ReadBook)
