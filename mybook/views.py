5dac7e120585f1eab008b86f3fc52c4c50353bc5
kronek <kronkowski.daniel@gmail.com>
2023-12-14 20:01:48 +0100

    #3 add adding books to user shelf

--- mybook/views.py
+++ mybook/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, DetailView, ListView
 
-from .models import Book
+from .models import Book, UserShelf
+from .forms import AddToShelfForm
 
 
 def home(request):
@@ -14,7 +16,23 @@ def home(request):
 
 @login_required
 def user_view(request):
-    return render(request, 'mybook/profile.html')
+    try:
+        user_shelf = UserShelf.objects.get(user=request.user)
+
+        read_books = user_shelf.read_books.all()
+        to_read_books = user_shelf.to_read_books.all()
+
+        context = {
+            'read_books': read_books,
+            'to_read_books': to_read_books,
+        }
+    except:
+        context = {
+            'read_books': 'empty',
+            'to_read_books': 'empty',
+        }
+
+    return render(request, 'mybook/profile.html', context)
 
 
 class SignUpView(CreateView):
@@ -31,12 +49,36 @@ class BookCreate(LoginRequiredMixin,CreateView):
 
 class BookDetail(DetailView):
     model=Book
+    context_object_name = 'book_detail'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         book = context['object']
         context['genres'] = book.genre.all()
         return context
+
+    def post(self, request, *args, **kwargs):
+        action = request.POST.get('action')
+
+        if action == 'add_to_read':
+            self.add_book_to_shelf('read', 'Read')
+        elif action == 'add_to_to_read':
+            self.add_book_to_shelf('to_read', 'To Read')
+        else:
+            messages.warning(request, 'Invalid action.')
+
+        return redirect('mybook:book_detail', pk=self.kwargs['pk'])
+
+    def add_book_to_shelf(self, status, shelf_name):
+        book = self.get_object()
+        user_shelf, created = UserShelf.objects.get_or_create(user=self.request.user)
+
+        if status == 'read':
+            user_shelf.read_books.add(book)
+            messages.success(self.request, f'Added "{book.title}" to your {shelf_name} books.')
+        elif status == 'to_read':
+            user_shelf.to_read_books.add(book)
+            messages.success(self.request, f'Added "{book.title}" to your {shelf_name} books.')
 
 
 class BookList(ListView):
