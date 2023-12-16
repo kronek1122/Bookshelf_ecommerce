cec63ad52b986d04a41916b0bc5f85e22c5995db
kronek <kronkowski.daniel@gmail.com>
2023-12-16 21:16:55 +0100

    #3 add settings page, add change password

--- mybook/views.py
+++ mybook/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, DetailView, ListView
 
@@ -33,6 +34,27 @@ def user_view(request):
         }
 
     return render(request, 'mybook/profile.html', context)
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Your password was successfully updated')
+            return redirect('/mybook/settings')
+        else:
+            messages.error(request, 'Please correct the error below')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'mybook/change_password.html', {
+        'form':form
+    })
+
+
+def user_settings(request):
+    return render(request, 'mybook/setting_page.html')
 
 
 class SignUpView(CreateView):
@@ -84,3 +106,5 @@ class BookDetail(DetailView):
 class BookList(ListView):
     model=Book
     context_object_name = 'book_list'
+
+
