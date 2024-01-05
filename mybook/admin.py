1a83d217feb910247f512fa23528e5a544374d6b
kronek <kronkowski.daniel@gmail.com>
2024-01-05 14:41:14 +0100

    fix relation between Book and BookOpinion

--- mybook/admin.py
+++ mybook/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Genre, Author, Book, UserShelf, ReadBook
+from .models import Genre, Author, Book, UserShelf, BookOpinion
 
 
 admin.site.register(Genre)
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(UserShelf)
-admin.site.register(ReadBook)
+admin.site.register(BookOpinion)
--- mybook/models.py
+++ mybook/models.py
@@ -15,7 +15,7 @@ class Book(models.Model):
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     isbn = models.CharField('ISBN', max_length=13, unique=True)
     genre = models.ManyToManyField(Genre)
-    opinions = models.ManyToManyField('ReadBook', related_name='books_opinion')
+    book_opinion = models.ManyToManyField('BookOpinion')
 
 
     def __str__(self):
@@ -38,13 +38,13 @@ class UserShelf(models.Model):
     def __str__(self):
         return f"{self.user.username}'s Bookshelf"
 
-
-class ReadBook(models.Model):
+class BookOpinion(models.Model):
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
     shelf = models.ForeignKey(UserShelf, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     read_date = models.DateField(null=True, blank=True)
     review = models.TextField(blank=True)
     rating = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.shelf.user.username} read {self.book.title} on {self.read_date}"
+        return f"{self.shelf.user.username} read {self.book_id.title} on {self.read_date}"
+
