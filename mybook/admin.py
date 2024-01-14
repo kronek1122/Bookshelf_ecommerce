c5107c0aef87f2788cef39d84416b5943316625d
kronek <kronkowski.daniel@gmail.com>
2024-01-14 18:15:08 +0100

    register Messages

--- mybook/admin.py
+++ mybook/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Genre, Author, Book, UserShelf, BookOpinion
+from .models import Genre, Author, Book, UserShelf, BookOpinion, Messages
 
 
 admin.site.register(Genre)
@@ -8,3 +8,4 @@ admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(UserShelf)
 admin.site.register(BookOpinion)
+admin.site.register(Messages)
