0ec55b61d79240eeda2a681dff21f5d1578950ff
kronek <kronkowski.daniel@gmail.com>
2024-01-27 14:55:45 +0100

    add FollowList to urls.py

--- mybook/urls.py
+++ mybook/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
     path('change_username/', views.UsernameChangeView.as_view(), name = 'change_username'),
     path('change_user_name/done/', views.UsernameChangeDoneView.as_view(), name = 'change_username_done'),
     path('search_result/', views.search_bar, name='search_result'),
+    path('follow_list/', views.FollowList.as_view(), name='follow_list'),
 ]
