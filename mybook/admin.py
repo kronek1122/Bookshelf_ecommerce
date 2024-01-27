7408facaa74b03413dcb1715a031e2db42f939c0
kronek <kronkowski.daniel@gmail.com>
2024-01-27 16:43:28 +0100

    rm Messagess, add Post

--- mybook/admin.py
+++ mybook/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Genre, Author, Book, UserShelf, BookOpinion, Messages, UserFollow
+from .models import Genre, Author, Book, UserShelf, BookOpinion, UserFollow, Post
 
 
 admin.site.register(Genre)
@@ -8,5 +8,5 @@ admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(UserShelf)
 admin.site.register(BookOpinion)
-admin.site.register(Messages)
 admin.site.register(UserFollow)
+admin.site.register(Post)
