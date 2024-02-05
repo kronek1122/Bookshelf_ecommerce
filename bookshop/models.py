27a98473ade0c6afd2fff971927fdcde54392e2c
kronek <kronkowski.daniel@gmail.com>
2024-02-05 19:08:22 +0100

    create BookInventory class

--- bookshop/models.py
+++ bookshop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from mybook.models import Book
 
-# Create your models here.
+
+class BookInventory(models.Model):
+    book = models.OneToOneField(Book, on_delete=models.CASCADE, primary_key=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
