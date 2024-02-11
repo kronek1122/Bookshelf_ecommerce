ec1d23fb2eef76091eba0890fdd7d7cf564cec03
kronek <kronkowski.daniel@gmail.com>
2024-02-11 13:55:28 +0100

    register UserShoppingCart"

--- bookshop/admin.py
+++ bookshop/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import BookInventory
+from .models import BookInventory, UserShoppingCart
 
 
 admin.site.register(BookInventory)
+admin.site.register(UserShoppingCart)
+
