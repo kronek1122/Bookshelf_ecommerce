b09a00166f38950b670e9c64d2e520ab523aef29
kronek <kronkowski.daniel@gmail.com>
2024-03-01 09:38:41 +0100

    add BookInventory class

--- bookshop/views.py
+++ bookshop/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from .models import BookInventory
+
+class BookInventory(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = BookInventory
+    success_url = reverse_lazy('bookshop:create_book')
+    fields = ['book', 'price', 'quantity']
+    
+    def test_func(self):
+        return self.request.user.is_authenticated and self.request.user.is_staff
