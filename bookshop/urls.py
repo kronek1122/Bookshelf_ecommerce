00b9cbb48a67299582e29e891144e7c55a840eb9
kronek <kronkowski.daniel@gmail.com>
2024-03-01 09:38:07 +0100

    add create_book urls

--- bookshop/urls.py
+++ bookshop/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name = 'boohshop'
+
+app_name = 'bookshop'
 urlpatterns = [
+    path('create_book/', views.BookInventory.as_view(), name='create_book'),
+
 
 ]
