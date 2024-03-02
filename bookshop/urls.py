827ae7e10f10f727e245fb972f6798a7ffabaa29
kronek <kronkowski.daniel@gmail.com>
2024-03-02 22:09:42 +0100

    add shopping cart url

--- bookshop/urls.py
+++ bookshop/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'bookshop'
 urlpatterns = [
     path('create_book/', views.BookInventory.as_view(), name='create_book'),
-
+    path('shopping_cart/', views.shopping_cart, name='shopping_cart'),
 
 ]
